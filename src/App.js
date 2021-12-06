54e3d3e04ff31865824444834862a6eaf06965e7
Jon Tran <trungt8998@gmail.com>
2021-12-06 09:15:16 -0600

    redux walk around

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import store from "./store";
+import { bugAdd, bugRemove, bugResolve } from "./action";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  // componentDidMount() {
+  //   store.dispatch(action);
+  // }
+
+  componentWillMount() {
+    const unsubscribe = store.subscribe(() => {
+      console.log("store changed");
+    });
+    store.dispatch(bugAdd);
+    store.dispatch(bugResolve(1));
+    unsubscribe();
+    // store.dispatch(bugRemove);
+  }
+
+  render() {
+    return <div>{console.log(store.getState())}</div>;
+  }
 }
 
 export default App;
